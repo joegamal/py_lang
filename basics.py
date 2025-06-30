d71b0dc31d209fcd309a2279c698c285e4e77b7c
joegamal <yusufmagamalth@gmail.com>
2025-06-30 08:28:12 +0300

    data types and loops

---
+++ basics.py
@@ -0,0 +1,22 @@
+
+# data types in python
+
+num = 10
+
+print(type(num))
+
+float_mun = 10.5
+
+print(type(float_mun))
+
+complex_num = 2 + 3j
+
+print(type(complex_num))
+
+text = "rerere"
+
+print(type(text))
+
+cond = True
+
+print(type(cond))
---
+++ loops.py
@@ -0,0 +1,15 @@
+#loops in python
+
+
+for i in range(5,10):
+    print(i)
+
+
+
+i = 0
+
+while i !=  50:
+    print(i)
+    i += 1
+
+    
